13600bafea4d1380d289901b75872a71ed3e8861
salis <salisshuaibu11@gmail.com>
2021-10-17 20:08:31 +0000

    Wrote code for the server

---
+++ server.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+
+// Routes
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+    PORT,
+    console.log(`Server running on port ${PORT}`)
+);
